a0bfaa108de10782fae82ef78c668006e5a7c727
Andrew Barrett <andrew1094@gmail.com>
2019-05-16 00:20:12 -0500

    Squished some bugs, but i do notice that sometimes the app does not get the device's last known location which causes the app to not work. There MainActivity may need some refactoring...
    Signed-off-by: Andrew Barrett <andrew1094@gmail.com>

--- app/src/main/java/com/krakenjaws/findfood/Constants.java
+++ app/src/main/java/com/krakenjaws/findfood/Constants.java
@@ -17,5 +17,4 @@ public class Constants {
     public static final int ERROR_DIALOG_REQUEST = 9001;
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002;
     public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9003;
-
 }
